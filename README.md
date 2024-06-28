1fccc28390994f58ac4ee7769067ea4c37d35638
MelvinBrem <melvinbrem@live.com>
2024-06-28 08:59:45 +0200

    🎨

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://my.wpengine.com/api_access
+https://wpengineapi.com/reference
--- index.php
+++ index.php
@@ -10,68 +10,54 @@ require_once 'vendor/autoload.php';
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/.env');
 
-if (file_exists('output/sites.php')) {
-    require_once 'output/sites.php';
-}
-
 if (!empty($_GET['action'])) {
-    switch ($_GET['action']) {
-        case 'getinstalls':
-            get_installs();
-            break;
-        case 'getdomainsdns':
-            if (file_exists('output/sites.php')) {
-                require_once 'output/sites.php';
-            }
-            if (!empty($installs)) {
-                get_domains_dns($installs);
-            }
-            break;
-        case 'clean':
-            if (file_exists('output/sites.php')) {
-                require_once 'output/sites.php';
-            }
-            if (!empty($installs)) {
-                clean_installs($installs);
-            }
-            break;
-        case 'tocsv':
-            if (file_exists('output/sites_clean.php')) {
-                require_once 'output/sites_clean.php';
-            }
-            if (!empty($installs)) {
-                to_csv($installs);
-            }
-            break;
+    if ($_GET['action'] === 'to_csv') {
+        if (file_exists('output/sites.php')) {
+            require_once 'output/sites.php';
+        }
+        if (!empty($installs)) {
+            to_csv($installs);
+        } else {
+            throw new Exception("No installs");
+        }
+    } else if ($_GET['action'] === 'get_installs') {
+        get_installs();
+    } else {
+        throw new Exception("Invalid action");
     }
 } else {
-    $installs = get_installs();
-    get_domains_dns($installs);
-    clean_installs($installs);
-    to_csv($installs);
+    get_installs();
 }
 
-// Git installs
-function get_installs(): array
+
+// Git DNS records
+function get_installs(): void
 {
+    dump("Getting installs...");
+
     $ch = curl_init();
     $installs = [];
 
-    curl_setopt($ch, CURLOPT_URL, 'https://api.wpengineapi.com/v1/installs?limit=1000');
+    $limit = !empty($_GET['limit']) ? $_GET['limit'] : 999;
+    $offset = !empty($_GET['offset']) ? $_GET['offset'] : 0;
+
+    curl_setopt($ch, CURLOPT_URL, 'https://api.wpengineapi.com/v1/installs?limit=' . $limit . '&offset=' . $offset);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 
     $headers = array();
-    $cred_string = $_ENV['WPE_API_UN'] . ":" . $_ENV['WPE_API_PW'];
-    $headers[] = "Authorization: Basic " . base64_encode($cred_string);
+    $credString = $_ENV['WPE_API_UN'] . ":" . $_ENV['WPE_API_PW'];
+    $headers[] = "Authorization: Basic " . base64_encode($credString);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
     $result = curl_exec($ch);
     if (curl_errno($ch)) {
-        echo 'Error:' . curl_error($ch);
+        throw new Exception(curl_error($ch));
     }
 
     $data = json_decode($result, true);
+    if (empty($data['results'])) throw new Exception("No installs");
+
     foreach ($data['results'] as $result) {
         if ($result['environment'] !== 'production') continue;
 
@@ -80,88 +66,117 @@ function get_installs(): array
         ];
     }
 
-    // Save output to minimize API calls
+    // Dump to unused file for debugging purposes
     $installs_str = var_export($installs, true);
     $var = "<?php" . PHP_EOL . "\$installs = $installs_str;" . PHP_EOL . "?>";
-    fopen('output/sites.php', 'w');
-    file_put_contents('output/sites.php', $var);
+    fopen('output/all_sites.php', 'w');
+    file_put_contents('output/all_sites.php', $var);
 
-    return $installs;
-}
+    dump("Done");
+    dump("Total installs: " . count($installs));
 
-// Git DNS records
-function get_domains_dns(array $installs): void
-{
-    if (empty($installs)) throw new Exception("No installs");
+    dump("Getting DNS info...");
 
-    $ch = curl_init();
+    foreach ($installs as $key => $installData) {
+        $ch = curl_init();
 
-    foreach ($installs as &$install) {
-        curl_setopt($ch, CURLOPT_URL, 'https://api.wpengineapi.com/v1/installs/' . $install['id'] . '/domains');
+        curl_setopt($ch, CURLOPT_URL, 'https://api.wpengineapi.com/v1/installs/' . $installData['id'] . '/domains');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 
-        $headers = array();
-        $cred_string = '16f91498-4edb-434c-897d-93c2110d8e8d' . ":" . 'g4AfA4W5CjVzIrv1TAuG3lCYmz0dY44s';
-        $headers[] = "Authorization: Basic " . base64_encode($cred_string);
+        $headers = [];
+        $credString = $_ENV['WPE_API_UN'] . ":" . $_ENV['WPE_API_PW'];
+        $headers[] = "Authorization: Basic " . base64_encode($credString);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
         $result = curl_exec($ch);
         if (curl_errno($ch)) {
-            echo 'Error:' . curl_error($ch);
+            throw new Exception(curl_error($ch));
         }
 
-        $domainsResults = json_decode($result, true);
-        if (empty($domainsResults['results'])) continue;
+        $data = json_decode($result, true);
+        if (!empty($data['results'])) {
+            foreach ($data['results'] as $domain) {
+                if (str_contains($domain['name'], 'wpengine')) {
+                    continue;
+                }
 
-        foreach ($domainsResults['results'] as $domain) {
-            if (
-                str_contains($domain['name'], 'wpengine') ||
-                in_array($domain['name'], ['wpengine.com', 'wpengine.net']) ||
-                !empty($installs['domains']) && in_array($domain['name'],  $install['domains'])
-            ) {
-                continue;
-            };
+                $ch = curl_init();
 
-            $domainRecords = dns_get_record($domain['name'], DNS_A);
+                curl_setopt($ch, CURLOPT_URL, 'https://' . $domain['name']);
+                curl_setopt($ch, CURLOPT_HEADER, true);
+                curl_setopt($ch, CURLOPT_NOBODY, true);  // we don't need body
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_exec($ch);
+                $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-            $ipsToIgnore = ['141.193.213.20', '141.193.213.21'];
+                if ($responseCode !== 200) {
+                    continue;
+                }
 
-            if (!empty($domainRecords)) {
+                ob_start();
+                passthru("dig A +short {$domain['name']} | grep '^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$'");
+                $domainRecords = ob_get_contents();
+                ob_end_clean();
+
+                $domainRecords = explode(PHP_EOL, $domainRecords);
+                if (empty($domainRecords)) continue;
+
+                $ipsToIgnore = ['141.193.213.20', '141.193.213.21', '141.192.213.10', '141.192.213.11'];
                 foreach ($domainRecords as $domainRecord) {
-                    if (in_array($domainRecord['ip'], $ipsToIgnore)) {
-                        continue;
-                    }
+                    if (empty($domainRecord) || in_array($domainRecord, $ipsToIgnore)) continue;
 
-                    $install['domains'][$domain['name']] = $domainRecord['ip'];
+                    $installs[$key]['domains'][$domain['name']]['ip'] = $domainRecord;
                 }
             }
         }
 
+        // If no domains on this install are "real"
+        if (empty($installs[$key]['domains'])) {
+            unset($installs[$key]);
+        }
+
+        // Slows it down a little but so you can see that it's still working
         $installs_str = var_export($installs, true);
         $var = "<?php" . PHP_EOL . "\$installs = $installs_str;" . PHP_EOL . "?>";
         fopen('output/sites.php', 'w');
         file_put_contents('output/sites.php', $var);
     }
-}
 
-// Clean up empty/ installs with no DNS issues
-function clean_installs(array $installs): void
-{
-    if (empty($installs)) throw new Exception("No installs");
+    dump("Done");
+    dump("Total installs with \"real\" domains: " . count($installs));
 
-    if (empty($_GET['justclean']) || empty($_GET['justclean']) && $_GET['justclean'] !== 'true') {
-        foreach ($installs as $key => $install) {
-            if (empty($install['domains'])) {
-                unset($installs[$key]);
-            }
+    dump("Getting SSL expiration date...");
+
+    $currentDateTime = time();
+
+    foreach ($installs as &$install) {
+        foreach ($install['domains'] as $domainName => &$domainData) {
+
+            $get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE, "peer_name" => $domainName)));
+            $read = stream_socket_client(
+                "ssl://" . $domainName . ":443",
+                $errno,
+                $errstr,
+                30,
+                STREAM_CLIENT_CONNECT,
+                $get
+            );
+            $cert = stream_context_get_params($read);
+            $certinfo = openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
+
+            $expirationDateTime = $certinfo['validTo_time_t'];
+            $domainData['expired'] = $expirationDateTime <= $currentDateTime;
+            $domainData['expiration_date'] = date('Y-m-d H:i:s', $expirationDateTime);
         }
     }
 
     $installs_str = var_export($installs, true);
     $var = "<?php" . PHP_EOL . "\$installs = $installs_str;" . PHP_EOL . "?>";
-    fopen('output/sites_clean.php', 'w');
-    file_put_contents('output/sites_clean.php', $var);
+    fopen('output/sites.php', 'w');
+    file_put_contents('output/sites.php', $var);
+
+    dump("Done");
 }
 
 // Array to CSV, domain per row
@@ -169,20 +184,27 @@ function to_csv(array $installs): void
 {
     if (empty($installs)) throw new Exception("No installs");
 
-    $header = ['install', 'domain', 'current_ip'];
+    $header = ['install', 'domain', 'current_ip', 'expired', 'expiration_date'];
     $records = [];
 
     $csv = Writer::createFromString();
     $csv->insertOne($header);
 
     foreach ($installs as $installname => $installData) {
-        foreach ($installData['domains'] as $domain => $ip) {
-            $records[] = [$installname, $domain, $ip];
+        if (empty($installData['domains'])) continue;
+        foreach ($installData['domains'] as $domainName => $domainData) {
+            $records[] = [
+                $installname,
+                $domainName,
+                $domainData['ip'],
+                $domainData['expired'] ? 'yes' : 'no',
+                $domainData['expiration_date']
+            ];
         }
     }
 
     $csv->insertAll($records);
 
-    fopen('output/sites_clean.csv', 'w');
-    file_put_contents('output/sites_clean.csv', $csv->toString());
+    fopen('output/sites.csv', 'w');
+    file_put_contents('output/sites.csv', $csv->toString());
 }
